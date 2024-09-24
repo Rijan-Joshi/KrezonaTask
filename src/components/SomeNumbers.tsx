a81f97a6d4ee9abad73600fcbaa6215b5c341796
Rijan-Joshi <csaijspy@gmail.com>
2024-09-24 05:59:44 +0545

    Complete Stats

--- src/components/SomeNumbers.tsx
+++ src/components/SomeNumbers.tsx
@@ -1,7 +1,59 @@
+"use client";
 import React from "react";
+import Container from "./utilities/Container";
+import Section from "./utilities/Section";
+import Title from "./utilities/Title";
+import StatCard from "./stats/statCard";
+
+interface SomeNumbersProps {
+  id: number;
+  data: string;
+  description: string;
+  active: boolean;
+}
+
+const stats: SomeNumbersProps[] = [
+  {
+    id: 2,
+    data: "7.300+",
+    description: "The sky was cloudless",
+    active: false,
+  },
+
+  {
+    id: 2,
+    data: "420<",
+    description: "Deep dark Blue",
+    active: false,
+  },
+
+  {
+    id: 3,
+    data: "95%",
+    description: "Creating games",
+    active: true,
+  },
+
+  {
+    id: 4,
+    data: "0.002",
+    description: "Describe the desing",
+    active: false,
+  },
+];
 
 const SomeNumbers = () => {
-  return <div>SomeNumbers</div>;
+  return (
+    <Container className="text-center">
+      <Title>SOME NUMBERS</Title>
+      <Section title="Games Have a Consistent Look">
+        <div className="flex flex-col lg:flex-row space-x-5">
+          {stats.map((stat) => (
+            <StatCard key={stat.id} {...stat} />
+          ))}
+        </div>
+      </Section>
+    </Container>
+  );
 };
-
 export default SomeNumbers;
---
+++ src/components/stats/statCard.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+
+interface FeatureCardProps {
+  id: number;
+  data: string;
+  description: string;
+  active: boolean;
+}
+
+const statCard: React.FC<FeatureCardProps> = ({
+  id,
+  data,
+  description,
+  active,
+}) => (
+  <motion.div
+    className={`relative ${
+      active ? "bg-primary scale-[1.2]" : "bg-card"
+    }  w-[220px] h-[128px] rounded-lg cursor-pointer`}
+    whileHover={{ scale: 1.05, boxShadow: "0px 0px 8px rgb(79, 70, 229)" }}
+    transition={{ duration: 0.3 }}
+  >
+    <h1 className="font-primary text-[40px]">{data}</h1>
+    <p>{description}</p>
+  </motion.div>
+);
+
+export default statCard;
